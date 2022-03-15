5fa1025ad3fe3cedd666835ebeea1d3597edce69
AlbertNoMes <albertnotariomestres@gmail.com>
2022-03-15 13:47:51 +0100

    commit 1 acabado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BarrasuperiorComponent } from './barrasuperior/barrasuperior.component';
 import { TarjetaComponent } from './tarjeta/tarjeta.component';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BarrasuperiorComponent,
-    TarjetaComponent
+    TarjetaComponent,
+    AboutComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
