36d25fa527c802458a52239b147c188c59090e36
AlbertNoMes <albertnotariomestres@gmail.com>
2022-03-15 09:45:45 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Rick_Y_Morty_Frontend_App';
+}
